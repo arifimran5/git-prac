2558e43dfb367743a83228f51ce904913a7b4e32
arifimran5 <arifimran505@gmail.com>
2022-11-14 23:15:14 +0530

    add sayHello

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 function App() {
   //logic
 
+  const sayHello = () => {
+    console.log('hello');
+  };
+
   //ui
-  return <div>
-    <h1>Hello beautiful people</h1>
-  </div>;
+  return (
+    <div>
+      <h1>Hello beautiful people</h1>
+    </div>
+  );
 }
